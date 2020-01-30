4b9668c3f0acb9ded991565962ce599a483fe193
Hanna Svennevik <svennevikh@gmail.com>
2020-01-30 16:40:59 +0100

    Changed type of data to bool to reduce size

--- dataprep.py
+++ dataprep.py
@@ -11,7 +11,7 @@ Run script
 def read_t(t=0.25,root="./"):
     if t > 0.:
         data = pickle.load(open(root+'Ising2DFM_reSample_L40_T=%.2f.pkl'%t,'rb'))
-    return np.unpackbits(data).astype(int).reshape(-1,1600)
+    return np.unpackbits(data).astype(bool).reshape(-1,1600)
 
 stack = []
 for i,t in enumerate(np.arange(0.25,4.01,0.25)):
